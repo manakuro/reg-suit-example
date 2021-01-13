7d93b2c8808fb7ee7cee1b13d8d89c31fdb47dd8
manato.kuroda <a7108100@gmail.com>
2021-01-13 13:07:06 +0900

    ci: test

--- src/stories/Button.tsx
+++ src/stories/Button.tsx
@@ -39,7 +39,7 @@ export const Button: React.FC<ButtonProps> = ({
     <button
       type="button"
       className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
-      style={{ backgroundColor, padding: 24 }}
+      style={{ backgroundColor }}
       {...props}
     >
       {label}
